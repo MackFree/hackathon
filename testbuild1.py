28cedcada44ea06bafd15abf3bcc30211fe1c340
MackFree <michaelfreeman100@gmail.com>
2019-07-13 17:18:32 +1200

    Almost done, combining works and so does the ecosystem

--- testbuild1.py
+++ testbuild1.py
@@ -8,6 +8,10 @@ DATABASE = set()
 #initialising game engine
 pygame.init()
 
+#init game_state to menu
+#game state 0 = menu, 1 = game, 2 = help screen
+game_state = 0
+
 #setting the title
 pygame.display.set_caption("Learn About Elements!")
 
@@ -17,12 +21,64 @@ screen = pygame.display.set_mode(size)
 
 #resources inits go here
 
-#font
-font = pygame.font.Font('freesansbold.ttf', 13) 
-
 #colours in RGB
 white = 255, 255, 255
 black = 0, 0, 0
+
+#helpscreenimage
+helpscreenimage = pygame.image.load("assets/helpscreen.png")
+
+helpscreenimagerect = helpscreenimage.get_rect()
+
+#button image assets
+helpbutton = pygame.image.load("assets/button/helpbutton.png")
+gobutton = pygame.image.load("assets/button/startbutton.png")
+quitbutton = pygame.image.load("assets/button/quitbutton.png")
+combinebutton = pygame.image.load("assets/button/combinebutton.png")
+clearbutton = pygame.image.load("assets/button/clearbutton.png")
+backbutton = pygame.image.load("assets/button/backbutton.png")
+menubutton = pygame.image.load("assets/button/quittomenubutton.png")
+
+#button rects
+helpbuttonrect = helpbutton.get_rect()
+gobuttonrect = gobutton.get_rect()
+quitbuttonrect = quitbutton.get_rect()
+combinebuttonrect = combinebutton.get_rect()
+clearbuttonrect = clearbutton.get_rect()
+backbuttonrect = backbutton.get_rect()
+menubuttonrect = menubutton.get_rect()
+
+
+#button locations
+helpbuttonrect.x = 560
+gobuttonrect.x = 560
+quitbuttonrect.x = 560
+
+combinebuttonrect.x = 1050
+clearbuttonrect.x = 830
+menubuttonrect.x = 1050
+
+backbuttonrect.x = 560
+
+gobuttonrect.y = 200
+helpbuttonrect.y = 270
+quitbuttonrect.y = 340
+
+combinebuttonrect.y = 650
+clearbuttonrect.y = 650
+menubuttonrect.y = 30
+
+backbuttonrect.y = 650
+
+#text
+font = pygame.font.Font('freesansbold.ttf', 20)
+element_name_text = "Press Combine to see if you've made a compound"
+element_text_obj = font.render(element_name_text, True, black, white)
+element_text_rect = element_text_obj.get_rect()
+
+#text loc
+element_text_rect.x = 550
+element_text_rect.y = 45
 
 #drawing the rectangles needed, atom selection screen and place screen
 selection_rect = pygame.Rect(0, 0, 525, 719)
@@ -93,88 +149,162 @@ def read_database(db_file="database.txt"):
 def is_in_database(atom_combination):
     # sort the current atom combination by their name
     atom_sorted = ''.join([x.name for x in sorted(atom_combination)])
+    print(atom_sorted)
     return atom_sorted in DATABASE
 
-#game loop
 while 1:
-    #exiting game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: pygame.quit()
+    while game_state == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                if(gobuttonrect.collidepoint(pos)):
+                    #go to game
+                    game_state = 1
+                elif(quitbuttonrect.collidepoint(pos)):
+                    #quit the game
+                    pygame.quit()
+                elif(helpbuttonrect.collidepoint(pos)):
+                    #display help
+                    game_state = 2
         
         
-        #mousebutton down event
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            #getting the position
-            print(event.button)
+        #drawring
+        screen.fill(white)
+        screen.blit(quitbutton, quitbuttonrect)
+        screen.blit(gobutton, gobuttonrect)
+        screen.blit(helpbutton, helpbuttonrect)
+        
+        #making everything visible
+        pygame.display.flip()
+    
+    
+    
+    #getting data into program
+    read_database()
+    
+    #gameplay loop
+    while game_state == 1:
+        #exiting game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: pygame.quit()
+            
+            
+            #mousebutton down event
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #getting the position
+                pos = pygame.mouse.get_pos()
+                #now we need to check which atom it is clicked on
+                if(selection_rect.collidepoint(pos)):
+                    for atom in atom_array:
+                        #compare mousepos and atom circle pos
+                        if(atom.rect.collidepoint(pos)):
+                            if(event.button == 1):
+                                #find atom type and create new instance 
+                                #need atom constructor ect
+                                selected_atom = Atom(atom.name, atom.proton, atom.neutron, atom.electron, atom.image_loc, atom.info_loc)
+                            elif(event.button == 3):
+                                #now we gotta uhh disp text
+                                atom_info_img = atom.info
+            
+            
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if(simulation_rect.collidepoint(pos) and selected_atom is not None):
+                    #we in the simulation rect
+                    element_name_text = "Press Combine to see if you've made a compound"
+                    simulation_atoms.append(selected_atom)
+                selected_atom = None
+                atom_info_img = None
+                if(combinebuttonrect.collidepoint(pos)):
+                    #now we want to combine all the atoms
+                    in_database = is_in_database(simulation_atoms)
+                    if(in_database):
+                        element_name_text = "INSERT ELEMENT NAME HERE"
+                    else:
+                        element_name_text = "Sorry, this is not a compound"
+                
+                if(clearbuttonrect.collidepoint(pos)):
+                    simulation_atoms = []
+                    element_name_text = "Press Combine to see if you've made a compound"
+                
+                if(menubuttonrect.collidepoint(pos)):
+                    simulation_atoms = []
+                    game_state = 0
+                    
+        
+        #moving selected atom with the mouse
+        if(selected_atom is not None):
             pos = pygame.mouse.get_pos()
-            #now we need to check which atom it is clicked on
-            if(selection_rect.collidepoint(pos)):
-                print("EAT MY ASS")
-                for atom in atom_array:
-                    #compare mousepos and atom circle pos
-                    if(atom.rect.collidepoint(pos)):
-                        if(event.button == 1):
-                            #find atom type and create new instance 
-                            #need atom constructor ect
-                            selected_atom = Atom(atom.name, atom.proton, atom.neutron, atom.electron, atom.image_loc, atom.info_loc)
-                        elif(event.button == 3):
-                            #now we gotta uhh disp text
-                            atom_info_img = atom.info
-            if(simulation_rect.collidepoint(pos)):
-                print("Sim")
+            selected_atom.rect.x = pos[0] - (selected_atom.rect.width/2)
+            selected_atom.rect.y = pos[1] - (selected_atom.rect.width/2)
         
         
-        if event.type == pygame.MOUSEBUTTONUP:
+        
+        #re-drawring background
+        screen.fill(white)    
+        
+        #drawring #needs to be from the bottom up 
+        pygame.draw.rect(screen, black, selection_rect, 2)
+        
+        #now we draw an atom for each one in the array (Lets use a loop bc
+        #fuck doing it induvidiall
+        draw_pos_x = 25
+        draw_pos_y = 70
+        for atom in atom_array:
+            atom.rect.x = draw_pos_x
+            atom.rect.y = draw_pos_y
+            screen.blit(atom.image, atom.rect)
+            if(draw_pos_x != 400):
+                draw_pos_x += 125
+            else:
+                draw_pos_x = 25
+                draw_pos_y += 125
+        
+        for atom in simulation_atoms:
+            screen.blit(atom.image, atom.rect)
+        
+        if selected_atom is not None:
+            screen.blit(selected_atom.image, selected_atom.rect)    
+        
+        #text for atom info
+        if(atom_info_img is not None):
             pos = pygame.mouse.get_pos()
-            if(simulation_rect.collidepoint(pos) and selected_atom is not None):
-                #we in the simulation rect
-                simulation_atoms.append(selected_atom)
-            selected_atom = None
-            atom_info_img = None
-            #also need to place the atom in the simulator section
-    
-    #moving selected atom with the mouse
-    if(selected_atom is not None):
-        pos = pygame.mouse.get_pos()
-        selected_atom.rect.x = pos[0] - (selected_atom.rect.width/2)
-        selected_atom.rect.y = pos[1] - (selected_atom.rect.width/2)
-    
-    
-    
-    #re-drawring background
-    screen.fill(white)    
-    
-    #drawring #needs to be from the bottom up 
-    pygame.draw.rect(screen, black, selection_rect, 2)
-    
-    #now we draw an atom for each one in the array (Lets use a loop bc
-    #fuck doing it induvidiall
-    draw_pos_x = 25
-    draw_pos_y = 70
-    for atom in atom_array:
-        atom.rect.x = draw_pos_x
-        atom.rect.y = draw_pos_y
-        screen.blit(atom.image, atom.rect)
-        if(draw_pos_x != 400):
-            draw_pos_x += 125
-        else:
-            draw_pos_x = 25
-            draw_pos_y += 125
-    
-    for atom in simulation_atoms:
-        screen.blit(atom.image, atom.rect)
-    
-    if selected_atom is not None:
-        screen.blit(selected_atom.image, selected_atom.rect)    
-    
-    #text for atom info
-    if(atom_info_img is not None):
-        pos = pygame.mouse.get_pos()
-        infoRect = atom_info_img.get_rect()
-        infoRect.x = pos[0] - infoRect.width//2
-        infoRect.y = pos[1]
-        screen.blit(atom_info_img, infoRect)
+            infoRect = atom_info_img.get_rect()
+            infoRect.x = pos[0]
+            infoRect.y = pos[1]
+            screen.blit(atom_info_img, infoRect)
+        
+        #drawring combine button
+        screen.blit(combinebutton, combinebuttonrect)
+        screen.blit(clearbutton, clearbuttonrect)
+        screen.blit(menubutton, menubuttonrect)
+        
+        #text box shit
+        element_text_obj = font.render(element_name_text, True, black, white)
+        screen.blit(element_text_obj, element_text_rect)
+        
+        #making all redrawn things visible
+        pygame.display.flip()
     
     
-    #making all redrawn things visible
-    pygame.display.flip()
+    while game_state == 2:
+        #this is the help menu
+        
+        #exiting game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: pygame.quit()
+            
+            #checking if they want to exit
+            if(event.type == pygame.MOUSEBUTTONDOWN):
+                pos = pygame.mouse.get_pos()
+                if(backbuttonrect.collidepoint(pos)):
+                    #now we go back to gamestate 0
+                    game_state = 0
+        
+        screen.fill(black)
+        screen.blit(helpscreenimage, helpscreenimagerect)
+        screen.blit(backbutton, backbuttonrect)
+        
+        pygame.display.flip()
+        
